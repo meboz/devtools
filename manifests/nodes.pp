ca10f759576f7dee0fbc2f6a9a58d319b6a9c961
Byron Walker <byron.walker@angryagile.com.au>
2013-12-14 22:28:09 +1100

    Added notepad plus plus to my tools

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3,5 +3,9 @@ node default {
 }
 
 node "byron-pc.home" {
-	
+	package { 'notepadplusplus':
+        ensure          => installed,
+        provider        => 'chocolatey',
+        install_options => '-pre'
+    }	
 }
