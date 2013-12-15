f5ee37f96b97b59c5c5b6d02f9ed1bce466704ba
Byron Walker <byron.walker@angryagile.com.au>
2013-12-15 17:03:33 +1100

    testing a file

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3,6 +3,10 @@ node default {
 }
 
 node "byron-pc.home" {
+	file { 'c:/temo/puppet.txt':
+		ensure	=> present
+	}
+	
 	package { 'notepadplusplus.install':
         ensure          => installed,
         provider        => 'chocolatey',
